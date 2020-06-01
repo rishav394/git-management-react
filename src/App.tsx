d3f5d7b1d40ba20f096615773e4b92ce94ef693f
Rishav Rungta <rishav394@gmail.com>
2020-06-01 22:44:12 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App: React.FC = () => {
+	return (
+		<div>
+			<div>Show some love ❤️</div>
+			<div>
+				Support{' '}
+				<a href="https://github.com/rishav394/vit-faculty-search-react">this</a>{' '}
+				project with a star ⭐
+			</div>
+		</div>
+	);
+};
+
+export default App;
